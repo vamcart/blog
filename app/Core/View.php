3f02e9e814d3417359068eefe71634e3154c4686
vamcart <vamshop@gmail.com>
2026-06-02 10:41:42 +0300

    Добавлена папка cache

--- app/Core/View.php
+++ app/Core/View.php
@@ -5,98 +5,72 @@ namespace App\Core;
 class View
 {
     /**
-     * Template file name
+     * Template file name without extension
      */
     protected $file;
+
     /**
-     * Data to be passed into view
+     * Data passed to the template
      */
-    protected $data;
+    protected $data = [];
+
     /**
-     * Generated cached template file
+     * Smarty instance
      */
-    protected $cacheFile;
+    protected $smarty;
+
     /**
-     * The base path to view directory
+     * Base path to view directory
      */
     protected $basePath;
+
     /**
-     * Path to cache directory
+     * Path to cache/compiled templates
      */
     protected $cachePath;
 
-    /**
-     * Constructor
-     * @param $file string
-     * @param $data array data to be passed into view
-     * @throws \Exception
-     */
     public function __construct($file, $data = [])
     {
         $this->basePath = app()->getBasePath() . '/app/Views/';
-        $this->cachePath = app()->getBasePath() . '/cache/';
+        $this->cachePath = app()->getBasePath() . '/cache/smarty/';
+        if (!is_dir($this->cachePath)) {
+            mkdir($this->cachePath, 0775, true);
+        }
+
         $this->file = $file;
         $this->data = $data;
 
-        $file = $this->basePath . $this->file . '.html';
-        if (file_exists($file)) {
-            $content = file_get_contents($file);
-            $this->parse($content);
-        } else {
-            throw new \Exception('View ' . $file . ' not found!');
+        // init smarty
+        $this->smarty = new \Smarty\Smarty();
+        $this->smarty->setTemplateDir($this->basePath);
+        $this->smarty->setCompileDir($this->cachePath);
+        $this->smarty->setCacheDir($this->cachePath);
+
+        // register helper function plugins used in templates
+        $this->smarty->registerPlugin('function', 'config', function ($params, &$smarty) {
+            return config($params['key']);
+        });
+        $this->smarty->registerPlugin('function', 'url', function ($params, &$smarty) {
+            return url($params['path'] ?? '');
+        });
+        $this->smarty->registerPlugin('function', 'route', function ($params, &$smarty) {
+            return route($params['name'] ?? '', $params);
+        });
+        // expose isAdmin helper
+        $this->smarty->registerPlugin('function', 'isAdmin', function () {
+            return isAdmin();
+        });
+
+        // assign data to smarty
+        if (is_array($data)) {
+            foreach ($data as $k => $v) {
+                $this->smarty->assign($k, $v);
+            }
         }
     }
 
-    /**
-     * Parse the template tags inside a string and write parsed file into cache
-     * @param $content string
-     * @return void
-     */
-    protected function parse($content)
-    {
-        // single variables
-        $content = preg_replace('/{{\s+?((?:(?!}}).)+)\s+?}}/', '<?php echo htmlentities(\1) ?>', $content);
-        // print raw content
-        $content = preg_replace('/{!!\s+?((?:(?!\!\!}).)+)\s+?!!}/', '<?php echo \1 ?>', $content);
-        // php
-        $content = preg_replace('/\@php/', '<?php', $content);
-        $content = preg_replace('/\@endphp/', '?>', $content);
-        // loop
-        $content = preg_replace('/\@foreach\s+?\((.*)\)/', '<?php foreach (\1): ?>', $content);
-        $content = preg_replace('/\@endforeach/', '<?php endforeach; ?>', $content);
-
-        // conditions
-        $content = preg_replace('/\@if\s+?\((.*)\)/', '<?php if (\1): ?>', $content);
-        $content = preg_replace('/\@elseif\s+?\((.*)\)/', '<?php elseif (\1): ?>', $content);
-        $content = preg_replace('/\@else/', '<?php else: ?>', $content);
-        $content = preg_replace('/\@endif/', '<?php endif; ?>', $content);
-
-        $path = $this->cachePath . $this->file . '.php';
-        file_put_contents($path, $content);
-
-        $this->cacheFile = $path;
-    }
-
-    /**
-     * Get the parsed html
-     * @return string
-     */
     public function render()
     {
-        extract($this->data);
-        ob_start();
-        include $this->cacheFile;
-        $html = ob_get_clean();
-
-        return $html;
+        return $this->smarty->fetch($this->file . '.tpl');
     }
-
-    /**
-     * @return string
-     */
-    public function __toString()
-    {
-        return $this->render();
-    }
-
 }
